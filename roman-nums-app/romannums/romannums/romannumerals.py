cbd4083bcf0995dbf9fcdf9c7a0782900ab61113
multiplexer <edresner@gmail.com>
2015-10-14 18:51:44 -0400

    Initial commit of the Roman Numerals app.

---
+++ roman-nums-app/romannums/romannums/romannumerals.py
@@ -0,0 +1,112 @@
+# Requires PEP8 checking for style -- automatically enabled in pycharm.
+import unittest
+
+def recurse_d_to_r(val, roman, decimals, romanNumerals):
+
+    # Functional programming version.  We solve for the smallest case and then the common case.
+    # If val is less than the current head of the decimals list then we keep recursing the list.
+    # If val is equal or greater, we have a hit. We move the value from the roman numerals into our accumulator string and
+    # recurse with the same value until the val accumulator drops below the next decimal.
+    # This is much simpler do to with if/then/else but much longer. :)
+    # Another solution is to do it with mods and individual accumulators.
+
+    if decimals:
+        if val < decimals[0]:
+            # Recurse the tail
+            return recurse_d_to_r(val, roman, decimals[1:], romanNumerals[1:])
+        else:
+            # Remove the current decimal value from the val, attach the roman letter, recurse the data structure
+            return recurse_d_to_r(val-decimals[0], roman+romanNumerals[0], decimals, romanNumerals)
+    else:
+        return roman
+
+def convert_d_to_r(val):
+
+
+    # The setup.  This is all the valid 1 and 2 letter combos available.
+
+    decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+    if val < 0:
+        raise ValueError
+
+    if val > 3999:
+        raise ValueError
+
+    return recurse_d_to_r(val, "", decimals, romanNumerals)
+
+def convert_r_to_d(roman):
+    # Algorithm for convert roman to decimal is to read the string, reverse the string, read right from left.
+    # If the character is > than maximum, add to accumulator and increase maximum.
+    # If the character is < than maximum, decrease accumulator.
+
+    accumulator = 0
+    maximum = 0
+
+    lookup = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    for c in roman[::-1]:
+        if max(maximum, lookup[c]):
+            accumulator += lookup[c]
+            maximum = lookup[c]
+        else:
+            accumulator -= lookup[c]
+
+    return accumulator
+
+class DecToRomanTest(unittest.TestCase):
+
+    def testSmallest(self):
+        self.assertEqual('I', convert_d_to_r(1))
+
+    def testVI(self):
+        self.assertEqual('VI', convert_d_to_r(6))
+
+    def testIX(self):
+        self.assertEqual('IX', convert_d_to_r(9))
+
+    def testXVIII(self):
+        self.assertEqual('XVIII', convert_d_to_r(18))
+
+    def testXIX(self):
+        self.assertEqual('XIX', convert_d_to_r(19))
+
+    def testXXXVIII(self):
+        self.assertEqual('XXXVIII', convert_d_to_r(38))
+
+    def testXXXIX(self):
+        self.assertEqual('XXXIX', convert_d_to_r(39))
+
+    def testXL(self):
+        self.assertEqual('XL', convert_d_to_r(40))
+
+    def testXCVIII(self):
+        self.assertEqual('XCVIII', convert_d_to_r(98))
+
+    def testCCCLXXXVIII(self):
+        self.assertEqual('CCCLXXXVIII', convert_d_to_r(388))
+
+    def testCDXCIX(self):
+        self.assertEqual('CDXCIX', convert_d_to_r(499))
+
+    def testDCCCLXVII(self):
+        self.assertEqual('DCCCLXVII', convert_d_to_r(867))
+
+    def testMCMXCVIII(self):
+        self.assertEqual('MCMXCVIII', convert_d_to_r(1998))
+
+    def testMMMCMXCIX(self):
+        self.assertEqual('MMMCMXCIX', convert_d_to_r(3999))
+
+if __name__ == "__main__":
+
+    unittest.main()
---
+++ roman-nums-app/romannums/romannums/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .romannumerals import *
+from django.contrib import messages
+
+def index(request):
+
+    if request.method == "POST":
+
+        num = request.POST.get('decimal')
+
+        # Check if is an int
+        if not num.isdigit() or (int(num) < 1 and int(num) > 3999):
+            messages.error(request, 'Must be a number between 1 and 3999.')
+            return render(request, 'index.html')
+
+        # Run the conversion
+        try:
+            val = convert_d_to_r(int(request.POST.get('decimal')))
+            return render(request, 'index.html', {'value': val})
+        except ValueError:
+            return render(request, 'index.html')
+
+    else:
+        return render(request, 'index.html')
